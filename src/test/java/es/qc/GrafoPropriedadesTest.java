46f4da153e1f31e16d0ded16292c82bd0fd51843
Tiago <tiagoemonteiroo@gmail.com>
2025-05-12 20:14:57 +0100

    GrafoPropriedadesTest.java FIXED

--- src/test/java/es/qc/GrafoPropriedadesTest.java
+++ src/test/java/es/qc/GrafoPropriedadesTest.java
@@ -1,6 +1,9 @@
 package es.qc;
 
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,11 +40,17 @@ class GrafoPropriedadesTest {
     }
 
     @Test
-    void construirGrafoAutomaticamente() {
-        // Test that the graph is built automatically based on property intersections
+    void construirGrafoAutomaticamente() throws ParseException {
+        WKTReader reader = new WKTReader();
+
+        Geometry geom1 = reader.read("POLYGON((0 0, 2 0, 2 2, 0 2, 0 0))");
+        Geometry geom2 = reader.read("POLYGON((1 1, 3 1, 3 3, 1 3, 1 1))");  // Intersecta com geom1
+
         GrafoPropriedades grafo = new GrafoPropriedades();
-        Propriedade propriedade1 = new Propriedade("1", "001", 100.0, 200.0, "POINT(0 0)", null, "Owner", "Parish", "Municipality", "Island");
-        Propriedade propriedade2 = new Propriedade("2", "002", 150.0, 250.0, "POINT(1 1)", null, "Owner2", "Parish2", "Municipality2", "Island2");
+
+        Propriedade propriedade1 = new Propriedade("1", "001", 100.0, 200.0, geom1.toText(), geom1, "Owner", "Parish", "Municipality", "Island");
+        Propriedade propriedade2 = new Propriedade("2", "002", 150.0, 250.0, geom2.toText(), geom2, "Owner2", "Parish2", "Municipality2", "Island2");
+
         grafo.adicionarPropriedade(propriedade1);
         grafo.adicionarPropriedade(propriedade2);
 
