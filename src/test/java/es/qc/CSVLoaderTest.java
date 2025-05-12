678451ee80514a6e5b05ba7da028ea06b5c09c2e
Tiago <tiagoemonteiroo@gmail.com>
2025-05-12 21:13:43 +0100

    CSVLoaderTest.java FIXED

--- src/test/java/es/qc/CSVLoaderTest.java
+++ src/test/java/es/qc/CSVLoaderTest.java
@@ -3,6 +3,7 @@ package es.qc;
 import org.junit.jupiter.api.Test;
 
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,10 +36,10 @@ class CSVLoaderTest {
      * Valid CSV file with data.
      */
     @Test
-    void carregarPropriedades1() {
+    void carregarPropriedades1() throws Exception  {
         URL resource = getClass().getClassLoader().getResource("Madeira-Moodle-1.1.csv");
         assertNotNull(resource, "Test CSV file not found in resources.");
-        String validCSVPath = resource.getPath();
+        String validCSVPath = Paths.get(resource.toURI()).toString();
 
         Map<String, Propriedade> propriedades = assertDoesNotThrow(
                 () -> CSVLoader.carregarPropriedades(validCSVPath),
@@ -53,10 +54,10 @@ class CSVLoaderTest {
      * Empty CSV file.
      */
     @Test
-    void carregarPropriedades2() {
+    void carregarPropriedades2() throws Exception{
         URL resource = getClass().getClassLoader().getResource("empty.csv");
         assertNotNull(resource, "Empty CSV file not found in resources.");
-        String emptyCSVPath = resource.getPath();
+        String emptyCSVPath = Paths.get(resource.toURI()).toString();
 
         Map<String, Propriedade> propriedades = assertDoesNotThrow(
                 () -> CSVLoader.carregarPropriedades(emptyCSVPath),
